e4bfebb6289232c9316360cefba225129dddcdf3
Dave <davetherubix@gmail.com>
2024-03-04 15:24:01 +0100

    implement navbar responsive base

--- App/Frontend/accessibilator/src/layouts/DefaultLayout.tsx
+++ App/Frontend/accessibilator/src/layouts/DefaultLayout.tsx
@@ -5,10 +5,17 @@ import Button from '../components/UI/Button';
 import FeedbackForm from '../components/FeedbackForm/FeedbackForm';
 import { AuthContext } from '../contexts/AuthContext';
 import { IS_DEV_MODE } from '../configs/configs';
-import { Menu, Transition } from '@headlessui/react';
+import { Menu } from '@headlessui/react';
 import Image from 'next/image';
 import MagicEmailForm from '../components/MagicEmailForm/MagicEmailForm';
 import { ToastQueue } from '@react-spectrum/toast';
+import { Dialog, Disclosure, Transition } from '@headlessui/react';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
 
 // LayoutProps interface is defined to be used as the properties accepted by the DefaultLayout component
 interface LayoutProps {
@@ -29,147 +36,245 @@ const DefaultLayout: FC<LayoutProps> = ({ children, title, variant }) => {
   // useState hooks for managing local state in the component
   const [isShowingFeedback, setIsShowingFeedback] = useState(false);
   const [isShowingLoginModal, setIsShowingLoginModal] = useState(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   const router = useRouter();
 
   return (
     <>
       <div className='flex min-h-screen flex-col'>
-        <nav
-          className={`relative flex px-8 shadow ${
-            variant === 'slim' ? 'py-2' : 'py-4'
-          } `}
-        >
-          {/* Link component for navigation */}
-          <Link className='text-3xl font-extrabold uppercase' href='/'>
-            Accessibilator
-          </Link>
-          {/* Conditional rendering of the title */}
-          {title && (
-            <p className='absolute left-1/2 top-1/2 inline-block -translate-x-1/2 -translate-y-1/2 text-lg font-medium'>
-              {title}
-            </p>
-          )}
-          {/* Conditional rendering based on authentication status */}
-          {isAuthenticated ? (
-            // User menu for authenticated users
-            <Menu as='div' className='relative ml-auto'>
-              <div>
-                <Menu.Button className='flex rounded-full border-2 border-primary-400 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-primary-200'>
-                  <span className='sr-only'>Toggle user menu</span>
-                  <div className='inline-block h-9 w-9 overflow-hidden rounded-full'>
-                    <Image
-                      width={64}
-                      height={64}
-                      src={`https://ui-avatars.com/api/?name=${user?.email}&size=64&font-size=0.62&length=1&bold=true&background=431407&color=fafafa`}
-                      className='rounded-full '
-                      alt='profile name initials'
-                    />
-                  </div>
-                </Menu.Button>
-              </div>
-              {/** Render this Transition Layout component once the user is authenticated */}
-              <Transition
-                as={Fragment}
-                enter='transition ease-out duration-100'
-                enterFrom='transform opacity-0 scale-95'
-                enterTo='transform opacity-100 scale-100'
-                leave='transition ease-in duration-75'
-                leaveFrom='transform opacity-100 scale-100'
-                leaveTo='transform opacity-0 scale-95'
-              >
-                <Menu.Items className='absolute right-0 z-[200] mt-2 w-48 origin-top-right divide-y-2 rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-                  <Menu.Item>
-                    <Link
-                      href={'/user/presets'}
-                      className='block bg-white px-4 py-2 text-sm text-gray-800 hover:bg-primary-100'
-                    >
-                      My Settings
-                    </Link>
-                  </Menu.Item>
-                  <Menu.Item>
-                    <Link
-                      href={'/user/documents'}
-                      className='block bg-white px-4 py-2 text-sm text-gray-800 hover:bg-primary-100'
-                    >
-                      Past Documents
-                    </Link>
-                  </Menu.Item>
-                  <Menu.Item
-                    as='button'
-                    onClick={() => {
-                      logout();
-                      router.push('/');
-                      ToastQueue.neutral('User logged out', {
-                        timeout: 2000,
-                      });
-                    }}
-                    className={
-                      'w-full bg-white px-4 py-2 text-left text-sm text-gray-800 file:block hover:bg-primary-100'
-                    }
-                  >
-                    Log Out
-                  </Menu.Item>
-                </Menu.Items>
-              </Transition>
-            </Menu>
-          ) : (
-            // Render the Login menu for the users who are not authenticated
-            <Menu as='div' className='relative ml-auto'>
-              <div>
-                <Menu.Button className='btn-primary'>
-                  <span className='sr-only'>Toggle login menu</span>
-                  Login
-                </Menu.Button>
-              </div>
-              <Transition
-                as={Fragment}
-                enter='transition ease-out duration-100'
-                enterFrom='transform opacity-0 scale-95'
-                enterTo='transform opacity-100 scale-100'
-                leave='transition ease-in duration-75'
-                leaveFrom='transform opacity-100 scale-100'
-                leaveTo='transform opacity-0 scale-95'
-              >
-                <Menu.Items className='absolute right-0 z-[200] mt-2 w-48 origin-top-right divide-y-2 rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-                  <Menu.Item>
-                    <button
-                      onClick={() => {
-                        setIsShowingLoginModal(true);
-                      }}
-                      className='block w-full bg-white px-4 py-2 text-left text-sm text-gray-800 hover:bg-primary-100'
-                    >
-                      Login with email
-                    </button>
-                  </Menu.Item>
-
-                  {/** Conditionally render this component only when the application is in development mode */}
-                  {IS_DEV_MODE && (
+        <header>
+          <nav
+            className={`relative flex px-8 shadow ${
+              variant === 'slim' ? 'py-2' : 'py-4'
+            } `}
+          >
+            {/* Link component for navigation */}
+            <Link className='text-3xl font-extrabold uppercase' href='/'>
+              Accessibilator
+            </Link>
+            {/* Conditional rendering of the title */}
+            {title && (
+              <p className='absolute left-1/2 top-1/2 inline-block -translate-x-1/2 -translate-y-1/2 text-lg font-medium'>
+                {title}
+              </p>
+            )}
+            {/* Conditional rendering based on authentication status */}
+            {isAuthenticated ? (
+              // User menu for authenticated users
+              <Menu as='div' className='relative ml-auto hidden md:block'>
+                <div>
+                  <Menu.Button className='flex rounded-full border-2 border-primary-400 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-primary-200'>
+                    <span className='sr-only'>Toggle user menu</span>
+                    <div className='inline-block h-9 w-9 overflow-hidden rounded-full'>
+                      <Image
+                        width={64}
+                        height={64}
+                        src={`https://ui-avatars.com/api/?name=${user?.email}&size=64&font-size=0.62&length=1&bold=true&background=431407&color=fafafa`}
+                        className='rounded-full '
+                        alt='profile name initials'
+                      />
+                    </div>
+                  </Menu.Button>
+                </div>
+                {/** Render this Transition Layout component once the user is authenticated */}
+                <Transition
+                  as={Fragment}
+                  enter='transition ease-out duration-100'
+                  enterFrom='transform opacity-0 scale-95'
+                  enterTo='transform opacity-100 scale-100'
+                  leave='transition ease-in duration-75'
+                  leaveFrom='transform opacity-100 scale-100'
+                  leaveTo='transform opacity-0 scale-95'
+                >
+                  <Menu.Items className='absolute right-0 z-[200] mt-2 w-48 origin-top-right divide-y-2 rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
                     <Menu.Item>
                       <Link
+                        href={'/user/presets'}
                         className='block bg-white px-4 py-2 text-sm text-gray-800 hover:bg-primary-100'
-                        href={`${redirectUrl}`}
-                        target='_self'
                       >
-                        Continue with google
+                        My Settings
                       </Link>
                     </Menu.Item>
-                  )}
-                </Menu.Items>
-              </Transition>
-            </Menu>
-          )}
-          {/* Display the Send Feedback button */}
-          <Button
-            role='navigation'
-            variant='link'
-            className='ml-6 inline-block  px-6 py-2 text-base font-medium text-stone-900 underline'
-            text={'Send Feedback'}
-            onClick={() => {
-              setIsShowingFeedback(true);
-            }}
-          />
-        </nav>
+                    <Menu.Item>
+                      <Link
+                        href={'/user/documents'}
+                        className='block bg-white px-4 py-2 text-sm text-gray-800 hover:bg-primary-100'
+                      >
+                        Past Documents
+                      </Link>
+                    </Menu.Item>
+                    <Menu.Item
+                      as='button'
+                      onClick={() => {
+                        logout();
+                        router.push('/');
+                        ToastQueue.neutral('User logged out', {
+                          timeout: 2000,
+                        });
+                      }}
+                      className={
+                        'w-full bg-white px-4 py-2 text-left text-sm text-gray-800 file:block hover:bg-primary-100'
+                      }
+                    >
+                      Log Out
+                    </Menu.Item>
+                  </Menu.Items>
+                </Transition>
+              </Menu>
+            ) : (
+              // Render the Login menu for the users who are not authenticated
+              <Menu as='div' className='relative ml-auto hidden md:block'>
+                <div>
+                  <Menu.Button className='btn-primary'>
+                    <span className='sr-only'>Toggle login menu</span>
+                    Login
+                  </Menu.Button>
+                </div>
+                <Transition
+                  as={Fragment}
+                  enter='transition ease-out duration-100'
+                  enterFrom='transform opacity-0 scale-95'
+                  enterTo='transform opacity-100 scale-100'
+                  leave='transition ease-in duration-75'
+                  leaveFrom='transform opacity-100 scale-100'
+                  leaveTo='transform opacity-0 scale-95'
+                >
+                  <Menu.Items className='absolute right-0 z-[200] mt-2 w-48 origin-top-right divide-y-2 rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                    <Menu.Item>
+                      <button
+                        onClick={() => {
+                          setIsShowingLoginModal(true);
+                        }}
+                        className='block w-full bg-white px-4 py-2 text-left text-sm text-gray-800 hover:bg-primary-100'
+                      >
+                        Login with email
+                      </button>
+                    </Menu.Item>
+
+                    {/** Conditionally render this component only when the application is in development mode */}
+                    {IS_DEV_MODE && (
+                      <Menu.Item>
+                        <Link
+                          className='block bg-white px-4 py-2 text-sm text-gray-800 hover:bg-primary-100'
+                          href={`${redirectUrl}`}
+                          target='_self'
+                        >
+                          Continue with google
+                        </Link>
+                      </Menu.Item>
+                    )}
+                  </Menu.Items>
+                </Transition>
+              </Menu>
+            )}
+            {/* Display the Send Feedback button */}
+            <Button
+              role='navigation'
+              variant='link'
+              className='ml-6 hidden px-6 py-2 text-base font-medium text-stone-900 underline md:inline-block'
+              text={'Send Feedback'}
+              onClick={() => {
+                setIsShowingFeedback(true);
+              }}
+            />
+            <div className='ml-auto flex md:hidden'>
+              <button
+                type='button'
+                className='-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700'
+                onClick={() => setMobileMenuOpen(true)}
+              >
+                <span className='sr-only'>Open main menu</span>
+                <Bars3Icon className='h-7 w-7' aria-hidden='true' />
+              </button>
+            </div>
+          </nav>
+
+          <Dialog
+            as='div'
+            className='md:hidden'
+            open={mobileMenuOpen}
+            onClose={setMobileMenuOpen}
+          >
+            <div className='fixed inset-0 z-10' />
+            <Dialog.Panel className='fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'>
+              <div className='flex items-center justify-between'>
+                <Link
+                  className='-m-1.5 p-1.5 text-2xl font-extrabold uppercase'
+                  href='/'
+                >
+                  <span className='sr-only'>Accessibilator</span>
+                  Accessibilator
+                </Link>
+                <button
+                  type='button'
+                  className='-m-2.5 rounded-md p-2.5 text-gray-700'
+                  onClick={() => setMobileMenuOpen(false)}
+                >
+                  <span className='sr-only'>Close menu</span>
+                  <XMarkIcon className='h-7 w-7' aria-hidden='true' />
+                </button>
+              </div>
+              <div className='mt-6 flow-root'>
+                <div className='-my-6 divide-y divide-gray-500/10'>
+                  <div className='space-y-4 py-6'>
+                    <Link
+                      href='#'
+                      className='-mx-3 block rounded-lg px-3 py-2 text-lg font-semibold leading-7 text-stone-900 hover:bg-gray-50'
+                    >
+                      About
+                    </Link>
+                    <Button
+                      role='navigation'
+                      variant='link'
+                      className='px-0 py-2 text-lg font-semibold text-stone-900 '
+                      text={'Send Feedback'}
+                      onClick={() => {
+                        setIsShowingFeedback(true);
+                      }}
+                    />
+                  </div>
+                  <Disclosure as='div' className='-mx-3'>
+                    {({ open }) => (
+                      <>
+                        <Disclosure.Button className='mt-2 flex w-full items-center justify-between rounded-lg py-4 pl-3 pr-3.5 text-lg font-semibold leading-7 text-stone-900 hover:bg-gray-50'>
+                          Login
+                          <ChevronDownIcon
+                            className={classNames(
+                              open ? 'rotate-180' : '',
+                              'h-5 w-5 flex-none'
+                            )}
+                            aria-hidden='true'
+                          />
+                        </Disclosure.Button>
+                        <Disclosure.Panel className='mt-2 space-y-2'>
+                          <Disclosure.Button
+                            as={'button'}
+                            onClick={() => {
+                              setIsShowingLoginModal(true);
+                            }}
+                            className='block rounded-lg py-2 pl-6 pr-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
+                          >
+                            Login with email
+                          </Disclosure.Button>
+
+                          <Disclosure.Button
+                            as={Link}
+                            href={`${redirectUrl}`}
+                            target='_self'
+                            className='block rounded-lg py-2 pl-6 pr-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
+                          >
+                            Continue with google
+                          </Disclosure.Button>
+                        </Disclosure.Panel>
+                      </>
+                    )}
+                  </Disclosure>
+                </div>
+              </div>
+            </Dialog.Panel>
+          </Dialog>
+        </header>
         {children}
         {/* Render the Footer element (currently invisible) */}
         <footer className='invisible p-5'>Footer</footer>
